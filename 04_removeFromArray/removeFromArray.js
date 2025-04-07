584c9e1d0d6acec1cb1126757b5d3dd65a33024f
lukaadzic00 <luka.adzic.rs@gmail.com>
2025-04-07 22:06:21 +0200

    Uradjen 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...arg) {
+    let len = array.length;
+    let newArr = new Array();
+    for(let i = 0; i < len; i++) {
+        if(!arg.includes(array[i])) {
+            newArr.push(array[i]);
+        }
+    }
+
+    return newArr;
 };
 
 // Do not edit below this line
